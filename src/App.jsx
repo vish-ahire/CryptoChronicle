d9ee175c9aeab34d31a5d9d7cbddc4b207c3d04a
pranav.ahire <pranav.ahire@LAIMLDLT6218>
2025-01-23 13:54:53 +0530

    fix default route

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,44 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {Footer,History,Overview,Dashboard,Header} from './index'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Footer, History, Overview, Dashboard, Header } from './index';
 
 function App() {
-  const [theme, setTheme] = useState("light"); 
+  const [theme, setTheme] = useState("light");
+
   const toggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
     document.documentElement.classList.remove(theme);
     document.documentElement.classList.add(newTheme);
-    localStorage.setItem("theme", newTheme); 
+    localStorage.setItem("theme", newTheme);
   };
+
   useEffect(() => {
     const savedTheme = localStorage.getItem("theme");
     if (savedTheme) {
       setTheme(savedTheme);
-      document.documentElement.classList.add(savedTheme); 
+      document.documentElement.classList.add(savedTheme);
     }
   }, []);
+
   return (
     <Router>
       <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow overflow-auto pt-24 pb-28 dark:bg-gray-900 dark:text-black">
-      <Routes>
-        <Route path="/Dashboard" element={<Dashboard />} />
-        <Route path="/overview" element={<Overview />} />
-        <Route path="/history" element={<History />} />
-      </Routes>
-      </main>
-      <Footer/>
-      <button
+        <Header />
+        <main className="flex-grow overflow-auto pt-24 pb-28 dark:bg-gray-900 dark:text-black">
+          <Routes>
+            <Route path="/" element={<Navigate to="/dashboard" />} /> {/* Default Route */}
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/overview" element={<Overview />} />
+            <Route path="/history" element={<History />} />
+          </Routes>
+        </main>
+        <Footer />
+        <button
           onClick={toggleTheme}
-          className="fixed bottom-4 right-4 p-3 mb-16 bg-gray-800 text-white rounded-full shadow-lg transition duration-300 hover:bg-gray-600 "
+          className="fixed bottom-4 right-4 p-3 mb-16 bg-gray-800 text-white rounded-full shadow-lg transition duration-300 hover:bg-gray-600"
         >
-          {theme === "light" ? (
-            <span>🌙</span> 
-          ) : (
-            <span>🌞</span> 
-          )}
+          {theme === "light" ? <span>🌙</span> : <span>🌞</span>}
         </button>
       </div>
     </Router>
